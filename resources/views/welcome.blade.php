b2e63eb7069968fbab4a65ae2f1fbe880722fdde
codeburstMst <51908203+codeburstMst@users.noreply.github.com>
2019-07-11 16:16:55 +0100

    Revert "Develop"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,60 +1,99 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-    <div class="container">
-        <div class="jumbotron">
-            <h1 class="display-4">Welcome to WeBlog</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="http://127.0.0.1:8000/create/post" role="button">Add post</a>
-        </div>
+        <title>Laravel</title>
 
-        @if(count($posts) > 0)
-            <div class="row">
-            @foreach($posts as $post)
-            <div class="col-lg-4">
-                <div class="card m-1">
-                    <img class="card-img-top" src="{{ Storage::url($post->image) }}" alt="Card image cap">
-                    <div class="card-body">
-                    <h5 class="card-title">{{ $post->title }}</h5>
-                    <p class="card-text">{{ str_limit($post->body, $limit = 150, $end = '...') }}</p>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-                    </div>
-                    <ul class='nav nav-pills card-body'>
-                        <li role='presentation'>
-                            <a href="{{url('post/'.$post->id.'/show')}}">
-                                <span>VIEW</span>
-                            </a>
-                            <a href="{{url('post/'.$post->id.'/edit')}}">
-                                <span>EDIT</span>
-                            </a>
-                            <a href="{{url('post/'.$post->id.'/delete')}}">
-                                <span>DELETE</span>
-                            </a>
-                        </li>
-                        <!-- <li role='presentation'>
-                            <a href="#">
-                                <span> EDIT</span>
-                            </a>
-                        </li>
-                        <li role='presentation'>
-                            <a href="#">
-                                <span> DELETE</span>
-                            </a>
-                        </li> -->
-                    </ul>
-                    <div class="card-footer">
-                    <small class="text-muted">{{ Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</small>
-                    </div>
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-            @endforeach
-            </div>
-        @else
-        <div class="alert alert-info" role="alert">
-            There are no posts yet.
         </div>
-        @endif
-    </div>
-@endsection
+    </body>
+</html>
