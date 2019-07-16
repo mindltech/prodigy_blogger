2f2f34773897a062c8ef8e6e8f800021354e2557
dimola7.github.io <didiogidan@gmail.com>
2019-07-16 13:36:36 +0100

    Add method for post update

--- resources/views/edit_post.blade.php
+++ resources/views/edit_post.blade.php
@@ -2,20 +2,23 @@
 
 @section('content')
     <div class='container'>
-        <form>
+        <form action="/post/{{ $post->id }}" enctype='multipart/form-data' method = "POST">
+        {{ csrf_field() }}
+        {{ method_field('PATCH') }}
+
             <div class="form-group">
                 <label for="exampleInputEmail1">Title</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" value='{{ $post->title }}' aria-describedby="emailHelp" placeholder="">
+                <input type="text" class="form-control" name="title" id="exampleInputEmail1" value='{{ $post->title }}' aria-describedby="emailHelp" placeholder="">
             </div>
 
             <div class="form-group">
     <label for="exampleFormControlTextarea1">Body</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" value="{{ str_limit($post->body, $limit = 150, $end = '...') }}" placeholder="Tell your story" rows="10">{{ str_limit($post->body, $limit = 150, $end = '...') }}</textarea>
+    <textarea class="form-control" id="exampleFormControlTextarea1" name="body"  value="{{ str_limit($post->body, $limit = 150, $end = '...') }}" placeholder="Tell your story" rows="10">{{ str_limit($post->body, $limit = 150, $end = '...') }}</textarea>
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlFile1">Change Image</label>
-    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+    <input type="file" class="form-control-file" name="image" id="exampleFormControlFile1">
   </div>
             <button type="submit" class="btn btn-primary">Update</button>
             </form>
