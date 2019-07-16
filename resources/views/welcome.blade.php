30b0a21c9cfcba801218f8d9380acece69110866
dimola7.github.io <didiogidan@gmail.com>
2019-07-16 14:27:52 +0100

    fixed conflicts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,14 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to WeBlog</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <a class="btn btn-primary btn-lg" href="http://127.0.0.1:8000/create/post" role="button">Add post</a>
+        </div>
 
         @if(count($posts) > 0)
             <div class="row">
@@ -52,7 +18,7 @@
                     <img class="card-img-top shadow p-3 mb-5 bg-white rounded" src="{{ Storage::url($post->image) }}" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">{{ $post->title }}</h5>
-                    <p class="card-text">{{ str_limit($post->body, $limit = 1500, $end = '...') }}</p>
+                    <p class="card-text">{{ str_limit($post->body, $limit = 150, $end = '...') }}</p>
 
                     </div>
                     <ul class='nav nav-pills card-body'>
@@ -71,9 +37,14 @@
                     <div class="card-footer">
                     <small class="text-muted">{{ Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</small>
                     </div>
-
                 </div>
             </div>
+            @endforeach
+            </div>
+        @else
+        <div class="alert alert-info" role="alert">
+            There are no posts yet.
         </div>
-    </body>
-</html>
+        @endif
+    </div>
+@endsection
