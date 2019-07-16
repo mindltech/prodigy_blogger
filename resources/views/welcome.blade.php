ed07f523e42cb8096d7b43e3f9a0a553f030b892
dimola7.github.io <didiogidan@gmail.com>
2019-07-16 14:14:21 +0100

    fixed conflicts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,6 @@
                 text-align: center;
             }
 
-<<<<<<< HEAD
         @if(count($posts) > 0)
             <div class="row">
             @foreach($posts as $post)
@@ -72,56 +71,7 @@
                     <div class="card-footer">
                     <small class="text-muted">{{ Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</small>
                     </div>
-=======
-            .title {
-                font-size: 84px;
-            }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
->>>>>>> ab5cdd8a9828d020c454e0d4425fd8e99feabf92
                 </div>
             </div>
         </div>
